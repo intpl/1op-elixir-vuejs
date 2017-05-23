e38f4bf8a899adcee8dc73716e72e72b11f09bb2
Bartek Gladecki <bgladecki@gmail.com>
2017-05-23 11:55:16 +0200

    typo

--- frontend/src/components/Logo.vue
+++ frontend/src/components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <span class="bigAndBold"><span class="grey"> / </span>1op.eu <span class="grey"> / </span></span>
-    <span><b>Extremely</b> private way to send you messages and passwords in realtime. Fully encrypted using <b>RSA-2048</b> for messages and <b>AES-256</b> to transmit their public keys.</span>
+    <span><b>Extremely</b> private way to send your messages and passwords in realtime. Fully encrypted using <b>RSA-2048</b> for messages and <b>AES-256</b> to transmit their public keys.</span>
     <div class="error" v-if="error">{{ error }}</div>
   </header>
 </template>
